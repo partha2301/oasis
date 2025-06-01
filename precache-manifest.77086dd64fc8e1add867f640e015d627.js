94cda147e52f1f356447403d8195bbd922fa10af
partha2301 <biswaspartha2301@gmail.com>
2025-06-01 17:31:07 +0530

    Updates

---
+++ precache-manifest.77086dd64fc8e1add867f640e015d627.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d479b3cc0d217d1f6cab6eaf76924706",
+    "url": "/oasis/index.html"
+  },
+  {
+    "revision": "f65fd3ae9aae47ff5d36",
+    "url": "/oasis/static/css/main.da00dbe6.chunk.css"
+  },
+  {
+    "revision": "6e3f08509e662084d485",
+    "url": "/oasis/static/js/2.03cfbe3b.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/oasis/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "f65fd3ae9aae47ff5d36",
+    "url": "/oasis/static/js/main.70f63a5c.chunk.js"
+  },
+  {
+    "revision": "392a4cbdd1e8b537233a",
+    "url": "/oasis/static/js/runtime~main.81fff13c.js"
+  },
+  {
+    "revision": "4e5093401ae43136f2afe89e77700ed9",
+    "url": "/oasis/static/media/logo.4e509340.png"
+  },
+  {
+    "revision": "3ce43a4587794419d79318f1e8aa7504",
+    "url": "/oasis/static/media/main.3ce43a45.jpg"
+  }
+]);
